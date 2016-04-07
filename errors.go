08cbae96b74cb03421f49f6b1982516b79196aa7
Joubin Muhammad Houshyar <alphazero@sensesay.net>
2016-04-07 17:17:24 -0400

    MOD remove prefix and add the basic package docs - TESTED

--- errors.go
+++ errors.go
@@ -1,3 +1,54 @@
+// package goerror provides for creation of semantic error types.
+// Using the stdlib error package, when trying to discern the error
+// type returned by a function, we can either (a) compare to a well-known
+// error reference (for example, io.EOF), or (b) we have to parse the
+// actual error message. The former, e.g. io.EOF, is fine for basic cases,
+// but obviously won't allow for call-site specific information in the error.
+//
+// For example, let's say we create an error for asserting input arg
+// correctness, something like an AsssertError, or IllegalArgumentError. We
+// can try pattern (a) per io.EOF, in which case we can certainly return that
+// error, but can't provide additional info such as which precise arg caused
+// the error. Or we can return a plain jane error with a formatted message,
+// in which case we can't immediately tell what 'kind' of error was returned.
+//
+// This package addresses this concern by providing error 'types' that can
+// be generically defined at (some) package level and then used with explicit
+// additional details.
+//
+// Errors are created using 'Define'. (Note, not 'New', since this merely
+// defines an error type).
+//
+//     var (
+//         TerribleError      = goerror.Define("TerribleError")
+//         NotSoTerribleError = goerror.Define("NotSoTerribleError")
+//     )
+//
+// Such error types can then be 'instantiated' using the defintion,
+// wherever one would normally create and/or return a generic error.
+//
+//     // function foo may return either TerribleError or
+//     // NotSoTerribleError
+//     func foo() error {
+//         // ...
+//         if flipcoin() {
+//            return TerribleError("an example usage")
+//         }
+//         return NotSoTerribleError() // detailed info is optional
+//     }
+//
+// And in the functional callsite, we can specifically check to see
+// what type of error we got.
+//
+//    if e := foo(); e != nil {
+//        switch typ := goerror.TypeOf(e): {
+//        case typ.Is(TerribleError):
+//            /* handle it */
+//        case typ.Is(NotSoTerribleError):
+//            /* handle it */
+//        }
+//    }
+//
 package goerror
 
 import (
@@ -5,19 +56,18 @@ import (
 )
 
 // ideally we want optional args capped at 1 item
-// but you can't do that in go.
+// but you can't do that in go. So the error generating
+// functions will create an error string that is simply
+// a concatenation of the args passed here.
 type errFn func(...string) error
 
 // need to export this to surface Is() to docs
 type ErrPredicate struct{ error }
 
-const errPrefix = "error - "
-const errPrefixLen = len(errPrefix)
-
 // defines a new categorical error.
 func Define(category string) errFn {
 	return func(args ...string) error {
-		errstr := errPrefix + category
+		errstr := category
 		if len(args) == 0 {
 			return errors.New(errstr)
 		}
